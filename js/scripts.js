558e657b835dd4b0420a6f47e2fa7414584eeb28
Yasmin Abdirahman <yasminosman37@yahoo.com>
2019-02-12 16:26:59 +0300

    operations added

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-};
-var number1 = prompt("Enter a number:");
-var number2 = prompt("Enter another number:");
-alert(add(number1, number2));
+
 var add = function(number1, number2) {
 return number1 + number2;
 };
 var number1=parseInt(prompt("Enter a number:"));
 var number2=parseInt(prompt("Enter another number:"));
 alert(add(number1, number2));
+var subtract=function(number1,number2){
+  return number1-number2;
+}
+var number1=parseInt(prompt("Enter a number"));
+var number2=parseInt(prompt("Enter another number"));
+alert(subtract(number1, number2));
+var multiply=function(number1,number2){
+  return number1*number2;
+}
+var number1=parseInt(prompt("Enter a number"));
+var number2=parseInt(prompt("Enter another number"));
+alert(multiply(number1, number2));
+var divide=function(number1,number2){
+  return number1/number2;
+}
+var number1=parseInt(prompt("Enter a number"));
+var number2=parseInt(prompt("Enter another number"));
+alert(divide(number1, number2));
